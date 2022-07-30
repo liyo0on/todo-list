dab5a4f1fe09d70aac00562849a9edfdb2242fe7
kambiz khomami <kambiizhkh@gmail.com>
2022-07-30 11:36:53 +0430

    [html css and js]for li and todolist pane

--- java/index.js
+++ java/index.js
@@ -1,8 +1,8 @@
-window.addEventListener('click',function (e) {
-    this.navigator.clipboard.writeText(e.target.className)
-    console.log(e.target)
-    console.log(e.target.parentElement)
-})
+// window.addEventListener('click',function (e) {
+//     this.navigator.clipboard.writeText(e.target.className)
+//     console.log(e.target)
+//     console.log(e.target.parentElement)
+// })
 
 const imageprofileElem = document.querySelector('#image--profile--import') 
 let upload_Image = ""
@@ -48,3 +48,74 @@ btnBackPage.forEach(element => {
         CreateFolderListElem.classList.remove('make-panels-invisible')
     })
 });
+
+let navCreateFolderListElem = document.querySelector('.create-folder')
+let navCreateTodoELEm = document.querySelector('.create-todo')
+
+navCreateTodoELEm.addEventListener('click',()=>{
+    CreateTodoListElem.classList.toggle('make-panels-invisible')
+})
+
+navCreateFolderListElem.addEventListener('click', ()=>{
+    CreateFolderListElem.classList.toggle('make-panels-invisible')
+})
+
+// create todo 
+let todoTitle = document.querySelector('#todo--title')
+let todoDescription = document.querySelector('#todo--description')
+let createTodoBtn = document.querySelector('.create--todo--btn')
+
+// insertAdjacentHTML
+let todoListPages = document.querySelector('.todo--list--page')
+
+let tableAllTodosElem = document.querySelector('.table-all-todos')
+let todoListArray = []
+
+class TODO {
+    constructor(title,Description){
+        this.title = title
+        this.Description = Description
+    }
+}
+
+
+createTodoBtn.addEventListener('click',()=>{
+    todoListArray.push(new TODO(todoTitle.value,todoDescription.value));
+    console.log(todoListArray);
+})
+
+createTodoBtn.addEventListener('click', ()=>{
+    let todoTitleValue = todoTitle.value
+    let todoDescriptionvalue = todoDescription.value
+
+    todoListPages.insertAdjacentHTML('afterbegin',`
+    <div class="todo-list-body--titele">
+    <div class="todo-tlis-titele">
+      <h2>title :</h2>
+      <h3 class="todo-list-value">${todoTitleValue}</h3>
+    </div>
+    <div class='todo--process'>
+      <h2>process : </h2>
+      <h3>
+        inprocess
+      </h3>
+    </div>
+  </div>
+  <p class="todo-list-body--description">${todoDescriptionvalue}
+  </p>
+  <div class="btn--contorol">
+    <button class="back--todo">back</button>
+    <button class="done--todo">done</button>
+    <button class="remove--todo">remove</button>
+  </div>`),
+  tableAllTodosElem.insertAdjacentHTML('afterbegin',`
+  <li class="table-all-todos-items">
+            <h2>
+              ${todoTitleValue}
+            </h2>
+              <i class="fa-solid fa-caret-right"></i>
+          </li>
+  `)
+
+    
+})
